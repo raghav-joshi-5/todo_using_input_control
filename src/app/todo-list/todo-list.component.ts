3174ae49b0754cf87727c095f22259ca624eebd9
raghav-joshi-5 <raghav.joshi2017@gmail.com>
2024-12-17 22:23:38 +0530

    todo_using_input_control

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Itodo } from '../const/todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+})
+export class TodoListComponent implements OnInit {
+  @ViewChild('todoItem') todoItem!: ElementRef;
+
+  generateUuid() {
+    return String('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx').replace(
+      /[xy]/g,
+      (character) => {
+        const random = (Math.random() * 16) | 0;
+        const value = character === 'x' ? random : (random & 0x3) | 0x8;
+
+        return value.toString(16);
+      }
+    );
+  }
+
+  todoArray: Array<Itodo> = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onBtn() {
+    let newObj = {
+      todoItem: this.todoItem.nativeElement.value,
+      todoId: this.generateUuid(),
+    };
+    console.log(newObj);
+    this.todoArray.push(newObj);
+  }
+}
